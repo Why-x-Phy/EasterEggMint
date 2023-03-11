7c5c0e8ef198c761278d7a59b25f29e0ab476c85
WhyPhy <100486824+Why-x-Phy@users.noreply.github.com>
2023-03-11 13:49:13 +0100

    all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
       <Head>
-        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <title>Unreveal Minting Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
